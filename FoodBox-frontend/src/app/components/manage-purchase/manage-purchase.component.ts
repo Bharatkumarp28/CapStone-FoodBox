d6c4dab9d027a46c1197eab1558d56df524d2660
Bharat Kumar P <bharatkumarp28@gmail.com>
2022-02-19 11:56:58 +0530

    Updated backEnd and front end

---
+++ FoodBox-frontend/src/app/components/manage-purchase/manage-purchase.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-purchase',
+  templateUrl: './manage-purchase.component.html',
+  styleUrls: ['./manage-purchase.component.css']
+})
+export class ManagePurchaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
